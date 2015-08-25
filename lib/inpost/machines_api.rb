5a9b0db1fa8f86918dbcfe6f88d037017f3c3fc7
Mateusz Baltyn <mateusz.baltyn@gmail.com>
2015-08-26 01:24:32 +0200

    Add cache in machine api

--- lib/inpost/machines_api.rb
+++ lib/inpost/machines_api.rb
@@ -4,31 +4,40 @@ module Inpost
 
     ENDPOINT_URL = 'https://api-pl.easypack24.net/v4/machines'
 
-    def initialize(endpoint_url: ENDPOINT_URL, cache_store: Inpost::CacheFileStore)
+    def initialize(endpoint_url: ENDPOINT_URL, cache_store: Inpost::CacheFileStore.new)
       @endpoint_url = endpoint_url
       @cache = cache_store
     end
 
     def machines
-      response = request(:get)
-      machine_collection = parse_response(response)
-      @cache.write(machine_collection)
+      get_from_cache || write_to_cache(get_from_http)
     end
 
     def machine(id)
-      machines = machines['_embedded']['machines'].find do |machine|
-        machine['id'] == id
-      end
+      machines.find { |machine| machine['id'] == id }
     end
 
-    private
+    protected
 
-    def request(method=:get)
-      RestClient::Request.execute(method: method, url: @endpoint_url)
+    def request(method=:get, url)
+      RestClient::Request.execute(method: method, url: url)
     end
 
     def parse_response(response)
-      JSON.parse(response)
+      JSON.parse(response)['_embedded']['machines']
+    end
+
+    def get_from_cache
+      return nil unless @cache
+      @cache.read
+    end
+
+    def write_to_cache(data)
+      @cache.write(data) if @cache
+    end
+
+    def get_from_http
+      parse_response(request(:get, @endpoint_url))
     end
 
   end
