d54dd2fd7dc251f0f9e38842bb6e3dd8e61958b3
Mateusz Baltyn <mateusz.baltyn@gmail.com>
2015-08-26 01:10:01 +0200

    Add cache file store

--- lib/inpost/cache_file_store.rb
+++ lib/inpost/cache_file_store.rb
@@ -1,13 +1,33 @@
 module Inpost
 
   class CacheFileStore
-    def initialize
+    FILENAME = '.inpost_cache'
+
+    def initialize(filename: FILENAME)
+      @filename = filename
+      @data = nil
     end
 
-    def write
+    def write(data)
+      File.write(@filename, serialize(data))
+      @data ||= data
     end
 
     def read
+      @data ||= unserialize(File.read(@filename))
+    rescue Errno::ENOENT
+      return nil
+    end
+
+    protected
+
+    def serialize(data)
+      YAML.dump({_cache_time: Time.now, data: data})
+    end
+
+    def unserialize(data)
+      YAML.load(data)[:data]
     end
   end
+
 end
